628984781ef7ec842a0cfcf82d2f36975d61c04c
Jwery <weryjoahchim@gmail.com>
2024-06-04 13:35:53 +0200

    - Created a new Counter component to manage the counter logic. - Imported and used the Counter component in HomeContainer. - Ensured that the counter stops at 0.

---
+++ src/containers/HomeContainer/Counter.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+//donner une valeur par défault quand on lance la page (0)
+function Counter() {
+  const [value, setValue] = useState(0);
+
+  //Une peremière variable pour incrémenter valur +1
+  const increment = () => {
+    setValue(value + 1);
+  };
+
+  //Une seconde variable pour décrémenter valeur -1 tout en évitant qu'elle devienne négative
+  const decrement = () => {
+    if (value > 1) {
+    setValue(value - 1);
+    } 
+    
+    //pour que le comteur s'arrêre à zero
+    else {
+      setValue(0);
+    }
+  };
+
+  return (
+    <div>
+      {/* afficher la variable (value) */}
+      <h2>{value}</h2>
+
+      {/* mettre le parametre (decrement) sur le onClick du premier boutton */}
+      <button onClick={decrement}>
+        -
+      </button>
+
+
+      {/* mettre le parametre (increment) sur le onClick du deuxième boutton */}
+      <button onClick={increment}>
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Counter from '../containers/HomeContainer/Counter';
 
 //donner une valeur par défault quand on lance la page (0)
 function Home() {
-  const [value, setValue] = useState(0);
-
-  //Une peremière variable pour incrémenter valur +1
-  const increment = () => {
-    setValue(value + 1);
-  };
-
-  //Une seconde variable pour décrémenter valeur -1
-  const decrement = () => {
-    setValue(value - 1);
-  };
-
+  
   return (
+
     <div>
-      {/* afficher la variable (value) */}
-      <h2>{value}</h2>
+      {/* appel du composant counter à la place de la logique de compteur */}
+     <Counter />
 
-      {/* mettre le parametre (decrement) sur le onClick du premier boutton */}
-      <button onClick={decrement}>
-        -
-      </button>
-
-
-      {/* mettre le parametre (increment) sur le onClick du deuxième boutton */}
-      <button onClick={increment}>
-        +
-      </button>
     </div>
   );
 }
