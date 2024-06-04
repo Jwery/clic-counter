25eecec4c7c214070049bfa00b20390c9e06d553
Jwery <weryjoahchim@gmail.com>
2024-06-04 12:44:47 +0200

    Add increment and decrement functionality to Home component
    - Implemented increment and decrement operations in the Home component.
    - Refactored the code for better readability and maintainability.
    
    Next step: Create a new component.

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Home({
-  onIncrement,
-  counter,
-}) {
+//donner une valeur par défault quand on lance la page (0)
+function Home() {
+  const [value, setValue] = useState(0);
+
+  //Une peremière variable pour incrémenter valur +1
+  const increment = () => {
+    setValue(value + 1);
+  };
+
+  //Une seconde variable pour décrémenter valeur -1
+  const decrement = () => {
+    setValue(value - 1);
+  };
+
   return (
     <div>
-      <button>
-        Vous avez cliqué {counter} fois
+      {/* afficher la variable (value) */}
+      <h2>{value}</h2>
+
+      {/* mettre le parametre (decrement) sur le onClick du premier boutton */}
+      <button onClick={decrement}>
+        -
+      </button>
+
+
+      {/* mettre le parametre (increment) sur le onClick du deuxième boutton */}
+      <button onClick={increment}>
+        +
       </button>
     </div>
   );
-};
+}
 
 export default Home;
